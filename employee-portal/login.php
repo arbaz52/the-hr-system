a88adb89de098f52fc54113a9ca972b9ee2adb16
Arbaz Ajaz <arbaz5256@gmail.com>
2021-06-21 20:43:59 +0500

    feat: employee login

--- employee-portal/login.php
+++ employee-portal/login.php
@@ -7,7 +7,7 @@
 
         <div class='col-4 mx-auto'>
 
-            <form class='form'>
+            <form class='form' onsubmit="handleFormSubmission(event)">
 
                 <h1>Login to<br /> Employee Dashboard</h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi accusantium dolores dolorem. Earum repellendus omnis totam, accusamus ab voluptatem necessitatibus!</p>
@@ -22,14 +22,51 @@
                     <input type='password' required placeholder="Enter Password" class='form-control' id='inputPassword' name='password' />
                 </div>
 
-                <div class='alert alert-primary mb-2'>If it is your first time login,  this will be used as your password.</div>
+                <div class='alert alert-primary mb-2'>If it is your first time login, this will be used as your password.</div>
                 <div class='form-group mb-3'>
-                    <a href="./index.php" class='btn btn-primary'>Login as Employee</a>
+                    <button class='btn btn-primary'>Login as Employee</button>
                 </div>
+
+                <div id='resultContainer'></div>
 
             </form>
         </div>
     </div>
 </div>
+
+
+<script>
+    function handleFormSubmission(event) {
+        event.preventDefault();
+
+        let email = inputEmail.value;
+        let password = inputPassword.value;
+
+        let input = {
+            email,
+            password,
+        }
+
+        fetch("http://localhost/the-hr-system/api/?action=EMPLOYEE_LOGIN", {
+            method: "POST",
+            body: JSON.stringify(input),
+            redirect: "error",
+            header: {
+                "Content-Type": "application/json",
+            },
+        }).then(res => res.json()).then((res) => {
+            console.debug(res)
+            if (res.error) {
+                resultContainer.innerHTML = `<div class='alert alert-danger'>${res.error}</div>`
+            } else if (res.success) {
+                setTimeout(() => {
+                    window.location.href = "./index.php";
+                }, 500)
+                resultContainer.innerHTML = `<div class='alert alert-success'>${res.success}<br />Redirecting...</div>`
+            }
+        })
+    }
+</script>
+
 
 </html>
